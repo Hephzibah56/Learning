487606059f4f5c9f0bbb79aab8aac0277629fb9f
Hephzibah Emereole <43935697+Hephzibah56@users.noreply.github.com>
2022-05-09 11:51:28 +0000

    db changes to both student and admin

--- zibah/admin/includes/config.php
+++ zibah/admin/includes/config.php
@@ -1,8 +1,9 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS' , getenv("MYSQLPASS")?? '');
+define('DB_PASS' , getenv("MYSQLPASS")?? "");
 define('DB_NAME', 'onlinecourse');
+
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 // Check connection
--- zibah/includes/config.php
+++ zibah/includes/config.php
@@ -4,8 +4,9 @@
 
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','onlinecourse');
+define('DB_PASS' , getenv("MYSQLPASS")?? "");
+define('DB_NAME', 'onlinecourse');
+
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
